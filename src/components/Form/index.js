26d07a3a498706115474ce9dc2052b3c861dc901
samuel.moceri <moceri.samuel@gmail.com>
2021-09-01 11:55:55 +0200

    Dispatch d'une action quadn on écrit dans le champ

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,12 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import './style.scss';
 
 const Form = () => {
   const messageInProgress = useSelector((state) => state.messageInProgress);
+  const dispatch = useDispatch();
+
+  const handleChange = () => {
+    dispatch({
+      type: 'CHANGE_MESSAGE',
+    });
+  };
 
   return (
     <form className="form">
-      <input className="form-input" type="text" value={messageInProgress} />
+      <input onChange={handleChange} className="form-input" type="text" value={messageInProgress} />
       <button className="form-btn" type="submit">Envoyer</button>
     </form>
   );
