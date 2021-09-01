37dbfdc1451b5e2c8cc853da620cf3ad08cda0d6
samuel.moceri <moceri.samuel@gmail.com>
2021-09-01 14:12:29 +0200

    envoie de message et ajout dans la liste ok

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,17 @@
 const initialState = {
-  listOfMessages: [{
-    id: 1,
-    author: 'salut',
-    message: 'coucou',
-  },
-],
-  messageInProgress: "il n'y a rien ici",
+  listOfMessages: [
+    {
+      id: 1,
+      author: 'Super Chat',
+      message: 'Coucou',
+    },
+    {
+      id: 2,
+      author: 'Super Chat',
+      message: 'T\'as pas des super-croquettes ?',
+    },
+  ],
+  messageInProgress: 'Ah ça march',
 };
 
 const reducer = (state = initialState, action = {}) => {
@@ -14,6 +20,22 @@ const reducer = (state = initialState, action = {}) => {
       return {
         ...state,
         messageInProgress: action.newMessage,
+      };
+    case 'SEND_MESSAGE':
+      // on respecte le principe d'immutability : on n'altère pas de valeur on en crée des nouvelles
+      // grace à ça mes outils vont pouvoir voir qu'il y eu des changements
+      // entre l'ancien le nouveau state et réagir
+      return { // mon nouveau state
+        ...state, // contient ce qu'il y avait déjà
+        messageInProgress: '', // + un message vide
+        listOfMessages: [ // + une liste de messages
+          ...state.listOfMessages, // contenant les messages actuels
+          { // + 1 nouveau
+            id: '3',
+            author: 'coucou',
+            message: state.messageInProgress,
+          },
+        ],
       };
     default:
       return state;
