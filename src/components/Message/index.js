7d8c8c13546d2738395fce40cc850ae710241bdb
samuel.moceri <moceri.samuel@gmail.com>
2021-09-01 10:13:42 +0200

    Passage des props

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -2,15 +2,16 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const Message = () => (
+const Message = ({ author, text }) => (
   <article className="message">
-    <p className="message-author">Auteur</p>
-    <p className="message-content">Bla bla bla</p>
+    <p className="message-author">{author}</p>
+    <p className="message-content">{text}</p>
   </article>
 );
 
 Message.propTypes = {
-
+  author: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default Message;
--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -3,12 +3,12 @@ import Message from 'src/components/Message';
 
 const Messages = () => (
   <section className="messages">
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
+    <Message author="Moi" text="coucou"/>
+    <Message author="Toi" text="coucou"/>
+    <Message author="Moi" text="ca va ?"/>
+    <Message author="Toi" text="bien et toi ?"/>
+    <Message author="Moi" text="tranquille"/>
+    <Message author="Toi" text="React"/>
   </section>
 );
 
