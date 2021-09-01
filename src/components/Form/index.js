b8992fbad095aea925d9a89963b78af6b83e9cf7
samuel.moceri <moceri.samuel@gmail.com>
2021-09-01 11:44:22 +0200

    début du champ controlé

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,15 @@
+import { useSelector } from 'react-redux';
 import './style.scss';
 
-const Form = () => (
-  <form className="form">
-    <input className="form-input" type="text" />
-    <button className="form-btn" type="submit">Envoyer</button>
-  </form>
-);
+const Form = () => {
+  const messageInProgress = useSelector((state) => state.messageInProgress);
+
+  return (
+    <form className="form">
+      <input className="form-input" type="text" value={messageInProgress} />
+      <button className="form-btn" type="submit">Envoyer</button>
+    </form>
+  );
+};
 
 export default Form;
